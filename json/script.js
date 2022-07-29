0febb5a47cc2e92808baa75dfc6bcbe55c071ec8
Sara2706 <101634428+Sara2706@users.noreply.github.com>
2022-07-29 00:17:15 -0700

    Add files via upload

---
+++ json/script.js
@@ -0,0 +1,15 @@
+// json content
+const jsonContent = {
+    "schoolName":"SBOA","Year":"1998","Rank":"1","Noofstudent":"1000"
+}
+
+// connect document and json
+document.write("<h1> Json example </h1>");
+// document.write("<br>");
+document.write(`<p>School name : ${jsonContent.schoolName} </p>`);
+// document.write("<br>");
+document.write(`<p>Year : ${jsonContent.Year} </p>`);
+// document.write("<br>");
+document.write(`<p>Rank in the city : ${jsonContent.Rank} </p>`);
+// document.write("<br>");
+document.write(`<p>No of students studying : ${jsonContent.Noofstudent} </p>`);
